fccc6babc3a684ce632eb66829ea374f21d9f5f8
TomaszMoroz <tmoroz688@gmail.com>
2024-12-29 13:24:03 +0100

    App: apply PageHeader

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <TopMenu />
+  <PageHeader />
 </template>
 
 <script setup>
 import './assets/css/main.css'
 import TopMenu from './components/TopMenu.vue'
+import PageHeader from './components/PageHeader.vue';
 
 </script>
 
